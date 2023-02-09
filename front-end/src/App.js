8f5360265b5949e36acae8835d03a42183d7da25
TIKO-Khatnaa <khatanbaatar622@gmail.com>
2023-02-09 19:35:06 +0800

    footer hiine

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,5 @@
 import './App.css';
-<<<<<<< HEAD
 import {HomePage} from './homepage';
-=======
-import HomePage from './homepage';
-import { Header } from './components/header';
->>>>>>> 9d2954877facb70574503559b32c70f994062145
 function App() {
   return (
     <div className="App">
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -2,46 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
 import { Post } from './components/post';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { HomePage } from './homepage';
 import reportWebVitals from './reportWebVitals';
-=======
-// import { Theme } from './provider/theme';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-// import { Blog } from './component/Blog';
-import { Header } from './components/header';
-// import { PostPage } from './component/post-page';
->>>>>>> 9d2954877facb70574503559b32c70f994062145
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-<<<<<<< HEAD
       <Routes>
         <Route path='/' element={<App />}></Route>
         <Route path='/blog/:id' element={<Post />}></Route>
         <Route path='/blog' element={<HomePage />}></Route>
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>
-);
-=======
-      {/* <Theme> */}
-        <Routes>
-          <Route path='/' element={<Header />}>
-            <Route index element={<App />} />
-            {/* <Route path="/Blog" element={<Blog />} /> */}
-            <Route path="/Home" element={<App />} />
-            {/* <Route path="/post/:id" element={<PostPage />} /> */}
-          </Route>
->>>>>>> 9d2954877facb70574503559b32c70f994062145
-
-        </Routes>
-      {/* </Theme> */}
-    </BrowserRouter>
-    {/* <TypesExample/> */}
   </React.StrictMode>
 );
